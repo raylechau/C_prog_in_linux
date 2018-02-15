ec164f9473f212d7c028a6032ae21fdeae7ba2a5
raylechau <raylechau@gmail.com>
2018-02-15 21:47:50 +0000

    2nd commit C_prog_in_linux

--- C_prog_in_linux/README.md
+++
@@ -1,1 +0,0 @@
-This directory is for all files related to C Programming in Linux , David Haskin 2008.
---
+++ chapter1_2.c
@@ -0,0 +1,21 @@
+/***********************************************************************
+C programming in Linux (c) David Haskin 2008
+chapter1_2.c
+***********************************************************************/
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("Hello, you are learning C\n");
+	printf("Number of arguments to the main function:%d\n", argc);
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argument number %d is %s\n", i, argv[i]);
+	}
+	return (0);
+}
